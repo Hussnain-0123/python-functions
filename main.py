d735fe1d705077c9ec8eb02659c6c805077024d9
Hussnain-0123 <hussnainkhawar82@gmail.com>
2025-03-28 12:25:10 +0500

    week days program

--- main.py
+++ main.py
@@ -1,2 +1,21 @@
-def sum():
-  return 1+1
+num=int(input("enter a number"))
+def week():
+    
+    if num==1:
+        print("monday")
+    elif num==2:
+        print("tuseday") 
+    elif num==3:
+        print("wednesday")
+    elif num==4:
+        print("thursday")
+    elif num==5:
+        print("friday")
+    elif num==6:
+        print("saturday")
+    elif num==7:
+        print("sunday")  
+    else:
+        print("invalid number ! enter a number between 1 to 7")   
+                           
+week()
